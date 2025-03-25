80f656c185696963b64157d7ad2f2970753656fa
kairo-18 <gladimirdungo@gmail.com>
2025-03-25 15:04:37 +0800

    added PhilSMS not semaphore

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -3,16 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http; // Import Http Facade
+use Illuminate\Support\Facades\Log;
 use App\Models\Sms;
+use App\Models\SmsAlertRegistration;
 
 class SmsController extends Controller
 {
-    public function sendSmsToAll(Request $request){
+    public function sendSmsToAll(Request $request)
+    {
 
-        Sms::create([
-            'type' => $request->type,
-            'message' => $request->message,
+        $contacts = SmsAlertRegistration::pluck('ContactNumber')->implode(",");
+
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . env('PHILSMS_API_KEY'),
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ])->post('https://app.philsms.com/api/v3/sms/send', [
+            'recipient' => $contacts,
+            'sender_id' => 'PhilSMS',
+            'type' => 'plain',
+            'message' => $request->type . ': ' . $request->message,
         ]);
-        return response()->json(['message' => 'SMS sent!']);
+
+        // Check the response
+        if ($response->successful()) {
+            Log::info('SMS sent successfully');
+        } else {
+            Log::error('Failed to send SMS');
+        }
     }
 }
