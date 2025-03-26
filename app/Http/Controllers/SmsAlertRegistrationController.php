5d08990ff8e3976a0e90b05db5688af687a07c43
root <root@LAPTOP-UUKCH0JO.>
2025-03-26 15:08:55 +0800

    fix sms alert

--- app/Http/Controllers/SmsAlertRegistrationController.php
+++ app/Http/Controllers/SmsAlertRegistrationController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\SmsAlertRegistration;
 
-class SmsAlertRegistration extends Controller
+class SmsAlertRegistrationController extends Controller
 {
     public function getSmsRegistration()
     {
